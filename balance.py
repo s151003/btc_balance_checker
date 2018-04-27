587e406378ef65f24f2dabe658dbede284a16d49
s151003 <s151003@kashiwanoha.ed.jp>
2018-04-28 02:31:08 +0900

    並列処理
    multiprocessing
    http://iatlex[.]com/python/parallel_first/

--- balance.py
+++ balance.py
@@ -1,8 +1,10 @@
 #encoding:utf-8
+from multiprocessing import Pool
+import random
 import requests
 import json
 
-address = "1Briu3soMHr9xygcnm81V66XDvMXnJwnbJ"
+address = ["1Briu3soMHr9xygcnm81V66XDvMXnJwnbJ","3Cbq7aT1tY8kMxWLbitaG7yT6bPbKChq64","3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r","16rCmCmbuWDhPjWTrpQGaU3EPdZF7MTdUk","32ixB1aXiwgpHGHo27SMRSULajCdc7jq9Q","1KjcmwEJ2iNkjanGzN4jWauN2Rsdk65GGu","12ib7dApVFvg82TXKycWBNpN8kFyiAN1dr","12tdYFDZ2kAtaHTiJwScYpAEah3bWrXFwr","1f1miYFQWTzdLiCBxtHHnNiW7WAWPUccr","1P1iThxBH542Gmk1kZNXyji4E4iwpvSbrt","1K5pv63rag715mN2REYcfMbQs7RyukPuSK","1JevQLpYsKAt2STFZWmQ8TTAfjSC8RTGdJ"]
 
 def bitflyer(address):
         r = requests.get("https://chainflyer.bitflyer.jp/v1/address/" + address)
@@ -32,9 +34,20 @@ def btccom(address):
         b = json.loads(r.text)
         return b['data']['received']
 
-print(bitflyer(address))
-print(spectro(address))
-print(blockchain(address))
-print(chainz(address))
-print(chainso(address))
-print(btccom(address))
+def balance(address):
+        rand = random.randint(0,6)
+        if rand == 0:
+                return bitflyer(address)
+        elif rand == 1:
+                return spectro(address)
+        elif rand == 2:
+                return blockchain(address)
+        elif rand == 3:
+                return chainz(address)
+        elif rand == 4:
+                return chainso(address)
+        elif rand == 5:
+                return btccom(address)
+if __name__ == '__main__':
+        p = Pool(4)
+        print(p.map(balance,address))
